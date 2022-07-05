57e0864c40a15ba37ed4283f8511b2923a4362fc
Mael PIGNET <you@example.com>
2022-07-05 14:10:36 +0200

    .

--- push_swap/sort_big.c
+++ push_swap/sort_big.c
@@ -1,22 +1,68 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_3.c                                           :+:      :+:    :+:   */
+/*   sort_big.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpignet <mpignet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 16:25:35 by mpignet           #+#    #+#             */
-/*   Updated: 2022/07/04 13:35:54 by mpignet          ###   ########.fr       */
+/*   Updated: 2022/07/05 14:05:16 by mpignet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	sort_int_tab(int *tab, int size)
+{
+	int	a;
+	int	tmp;
+
+	a = 1;
+	if (size != 0)
+	{
+		while (a <= size)
+		{
+			if (tab[a] < tab[a - 1])
+			{
+				tmp = tab[a];
+				tab[a] = tab[a - 1];
+				tab[a - 1] = tmp;
+				a = 0;
+			}
+			a++;
+		}
+	}
+}
+
+int	find_median(t_list *lst)
+{
+	int	median;
+	int	i;
+	int	*buff;
+	t_list	*tmp;
+
+	buff = (int)malloc(sizeof(int) * ft_lstsize(lst));
+	i = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		buff[i] = tmp->content;
+		tmp = tmp ->next;
+		i++;
+	}
+	sort_int_tab(buff, i);
+	median = buff[i / 2];
+	free(buff);
+	return (median);
+}
+
 void	sort_big(t_list **lst, t_list **b)
 {
-    int middle;
     t_list *move;
+	int		median;
 
-    middle = ft_lstsize(*lst) / 2;
+	median = find_median(&lst);
+	move = *lst;
+
     
 }
