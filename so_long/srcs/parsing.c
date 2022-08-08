bfa152e2fd3afbaa9f78257e4850c0764d54daf8
Mael PIGNET <you@example.com>
2022-08-08 17:53:54 +0200

    .

--- so_long/srcs/parsing.c
+++ so_long/srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mpignet <mpignet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 16:38:46 by mpignet           #+#    #+#             */
-/*   Updated: 2022/08/08 16:40:17 by mpignet          ###   ########.fr       */
+/*   Updated: 2022/08/08 16:51:12 by mpignet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,11 +54,14 @@ void	parsing(t_data *data, char *path)
 	if (fd == -1)
 		return (ft_putstr_fd("Error\nTrying to open an invalid file\n", 2));
 	buff = malloc(sizeof(char) * 100000);
+	if (!buff)
+		return ;
 	i = read(fd, buff, 100000);
 	if (i == -1)
 		return (ft_putstr_fd("Error\nTrying to read an invalid file\n", 2));
 	buff[i] = '\0';
 	data->map = ft_split(buff, '\n');
+	free(buff);
 	data->win_width = ft_strlen(data->map[0]) * 32;
 	data->win_height = get_height(data) * 32;
 	data->nbr_coll = get_nb_coll(data);
