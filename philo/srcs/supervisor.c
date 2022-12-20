a806f010579c67377ee3192522104fbf4cfae6c3
Mael PIGNET <you@example.com>
2022-12-20 15:18:31 +0100

    clean norm OK

--- philo/srcs/supervisor.c
+++ philo/srcs/supervisor.c
@@ -6,81 +6,80 @@
 /*   By: mpignet <mpignet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 15:00:58 by mpignet           #+#    #+#             */
-/*   Updated: 2022/12/19 17:26:26 by mpignet          ###   ########.fr       */
+/*   Updated: 2022/12/20 15:09:20 by mpignet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int need_to_stop_program(t_rul *rules)
+int	need_to_stop_program(t_rul *rules)
 {
-    pthread_mutex_lock(&rules->stop_prog_mt);
-    if (rules->stop_program == true)
-        return (pthread_mutex_unlock(&rules->stop_prog_mt), 1);
-    return (pthread_mutex_unlock(&rules->stop_prog_mt), 0);
+	pthread_mutex_lock(&rules->stop_prog_mt);
+	if (rules->stop_program == true)
+		return (pthread_mutex_unlock(&rules->stop_prog_mt), 1);
+	return (pthread_mutex_unlock(&rules->stop_prog_mt), 0);
 }
 
-int philo_death(t_philo *philo)
+void	set_stop_program_to_true(t_rul *rules)
 {
-    int time_since_last_meal;
-
-    pthread_mutex_lock(&philo->meals_mt);
-    time_since_last_meal = get_time() - philo->last_meal;
-    pthread_mutex_unlock(&philo->meals_mt);
-    if (time_since_last_meal >= philo->rules->lifespan)
-    {
-        print_action(philo, "died");
-        pthread_mutex_lock(&philo->rules->stop_prog_mt);
-        philo->rules->stop_program = true;
-        pthread_mutex_unlock(&philo->rules->stop_prog_mt);
-        return(1);
-    }
-    return (0);
+	pthread_mutex_lock(&rules->stop_prog_mt);
+	rules->stop_program = true;
+	pthread_mutex_unlock(&rules->stop_prog_mt);
 }
 
-int end_conditions(t_group *philos)
+int	philo_death(t_philo *philo)
 {
-    int     i;
-    int     max_meals;
+	int	time_since_last_meal;
 
-    i = -1;
-    max_meals = 0;
-    pthread_mutex_lock(&philos->rules->stop_prog_mt);
-    if (philos->rules->stop_program == true)
-        return (pthread_mutex_unlock(&philos->rules->stop_prog_mt), 1);
-    pthread_mutex_unlock(&philos->rules->stop_prog_mt);
-    while (++i < philos->rules->nb_philo)
-    {
-        if (philo_death(&philos->philo[i]))
-            return(1);
-        pthread_mutex_lock(&philos->philo[i].meals_mt);
-        if (philos->philo[i].nb_meals == philos->rules->meals_limit)
-            max_meals++;
-        pthread_mutex_unlock(&philos->philo[i].meals_mt);
-    }
-    if (max_meals == philos->rules->nb_philo)
-    {
-        pthread_mutex_lock(&philos->rules->stop_prog_mt);
-        philos->rules->stop_program = true;
-        pthread_mutex_unlock(&philos->rules->stop_prog_mt);
-        return (1);
-    }
-    return (0);
+	pthread_mutex_lock(&philo->meals_mt);
+	time_since_last_meal = get_time() - philo->last_meal;
+	pthread_mutex_unlock(&philo->meals_mt);
+	if (time_since_last_meal >= philo->rules->lifespan)
+	{
+		print_action(philo, "died");
+		set_stop_program_to_true(philo->rules);
+		return (1);
+	}
+	return (0);
 }
 
-void    *program_check(void *ptr)
+int	end_conditions(t_group *philos)
 {
-    t_group *philos;
+	int	i;
+	int	max_meals;
 
-    philos = (t_group *)ptr;
-    if (philos->rules->meals_limit == 0)
-    {
-        pthread_mutex_lock(&philos->rules->stop_prog_mt);
-        philos->rules->stop_program = true;
-        pthread_mutex_unlock(&philos->rules->stop_prog_mt);
-        return (NULL);
-    }
-    while (!end_conditions(philos))
-        usleep(100);
-    return (NULL);
+	i = -1;
+	max_meals = 0;
+	if (need_to_stop_program(philos->rules))
+		return (1);
+	while (++i < philos->rules->nb_philo)
+	{
+		if (philo_death(&philos->philo[i]))
+			return (1);
+		pthread_mutex_lock(&philos->philo[i].meals_mt);
+		if (philos->philo[i].nb_meals == philos->rules->meals_limit)
+			max_meals++;
+		pthread_mutex_unlock(&philos->philo[i].meals_mt);
+	}
+	if (max_meals == philos->rules->nb_philo)
+	{
+		set_stop_program_to_true(philos->rules);
+		return (1);
+	}
+	return (0);
+}
+
+void	*program_check(void *ptr)
+{
+	t_group	*philos;
+
+	philos = (t_group *)ptr;
+	if (philos->rules->meals_limit == 0)
+	{
+		set_stop_program_to_true(philos->rules);
+		return (NULL);
+	}
+	while (!end_conditions(philos))
+		usleep(100);
+	return (NULL);
 }
--- philo/srcs/time_print.c
+++ philo/srcs/time_print.c
@@ -6,7 +6,7 @@
 /*   By: mpignet <mpignet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 14:03:39 by mpignet           #+#    #+#             */
-/*   Updated: 2022/12/20 14:03:46 by mpignet          ###   ########.fr       */
+/*   Updated: 2022/12/20 15:04:38 by mpignet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_put_error(char *s)
 {
-    size_t	len;
+	size_t	len;
 
 	len = 0;
 	while (s[len])
@@ -22,40 +22,41 @@ void	ft_put_error(char *s)
 	write (2, s, len);
 }
 
-int ft_usleep(t_rul *rules, time_t time)
+int	ft_usleep(t_rul *rules, time_t time)
 {
-    time_t  waittime;
+	time_t	waittime;
 
-    waittime = get_time() + time;
-    while (get_time() < waittime)
-    {
-        if (need_to_stop_program(rules))
-            return (1);
-        usleep(10);
-    }
-    return (0);
+	waittime = get_time() + time;
+	while (get_time() < waittime)
+	{
+		if (need_to_stop_program(rules))
+			return (1);
+		usleep(10);
+	}
+	return (0);
 }
 
-time_t get_time(void)
+time_t	get_time(void)
 {
-    struct timeval  tv;
-    gettimeofday(&tv, NULL);
-    return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
-time_t time_stamp(time_t start_time)
+time_t	time_stamp(time_t start_time)
 {
-    return (get_time() - start_time);
+	return (get_time() - start_time);
 }
 
-int    print_action(t_philo *philo, char *msg)
+int	print_action(t_philo *philo, char *msg)
 {
-    if(need_to_stop_program(philo->rules))
-        return (1);
-    pthread_mutex_lock(&philo->rules->print_mt);
-    if(need_to_stop_program(philo->rules))
-        return (pthread_mutex_unlock(&philo->rules->print_mt), 1);
-    printf("%ld %d %s\n", time_stamp(philo->rules->start_time), philo->id, msg);
-    pthread_mutex_unlock(&philo->rules->print_mt);
-    return (0);
+	if (need_to_stop_program(philo->rules))
+		return (1);
+	pthread_mutex_lock(&philo->rules->print_mt);
+	if (need_to_stop_program(philo->rules))
+		return (pthread_mutex_unlock(&philo->rules->print_mt), 1);
+	printf("%ld %d %s\n", time_stamp(philo->rules->start_time), philo->id, msg);
+	pthread_mutex_unlock(&philo->rules->print_mt);
+	return (0);
 }
