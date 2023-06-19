b91387008f31715dde0c3eafb2791ed070d7790c
Mael PIGNET <you@example.com>
2023-06-19 14:56:39 +0200

    awd

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,10 +2,11 @@
 
 #create database and user for wordpress
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" -p$SQL_ROOT_PASSWORD
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" -p$SQL_ROOT_PASSWORD
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" -p$SQL_ROOT_PASSWORD
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" -p$SQL_ROOT_PASSWORD
-mysql -e "FLUSH PRIVILEGES;" -p$SQL_ROOT_PASSWORD
+sleep 5
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
