2e9d691effe990d8eb19efe0b52c76b14272112b
Mael PIGNET <you@example.com>
2023-06-20 19:05:32 +0200

    awdawda

--- inception/srcs/requirements/wordpress/tools/auto_config.sh
+++ inception/srcs/requirements/wordpress/tools/auto_config.sh
@@ -6,9 +6,7 @@ sleep 10
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
 	echo "Configuring wordpress..."
 	echo "${SQL_DATABASE}<->${SQL_USER}<->${SQL_PASSWORD}"
-	wp core download --allow-root
-	rm -f /var/www/html/wp-config.php
-	wp config create	--allow-root \
+	wp-cli.phar config create	--allow-root \
 						--url=mpignet.42.fr \
 						--dbname=$SQL_DATABASE \
 						--dbuser=$SQL_USER \
@@ -16,8 +14,8 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 						--dbhost=mariadb \
 						--path='/var/www/wordpress'
 sleep 4
-wp core install     --allow-root --url='mpignet.42.fr' --title='inception' --admin_user='mpignet' --admin_password='1234' --admin_email='mpignet20@gmail.com' --path='/var/www/wordpress'
-wp user create      --allow-root --role=author mpignet mpignet20@gmail.com --user_pass='1234' --path='/var/www/wordpress' >> /log.txt
+wp-cli.phar core install     --allow-root --url='mpignet.42.fr' --title='inception' --admin_user='mpignet' --admin_password='1234' --admin_email='mpignet20@gmail.com' --path='/var/www/wordpress'
+wp-cli.phar user create      --allow-root --role=author mpignet mpignet20@gmail.com --user_pass='1234' --path='/var/www/wordpress' >> /log.txt
 fi
 
 /usr/sbin/php-fpm7.3 -F
