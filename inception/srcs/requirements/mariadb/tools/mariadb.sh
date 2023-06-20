1b8f7d85e04173253eab32758ea657488b84d67a
Mael PIGNET <you@example.com>
2023-06-20 14:14:17 +0200

    awd

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,6 +3,7 @@
 #create database and user for wordpress
 service mysql start
 sleep 5
+echo "hello"
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
