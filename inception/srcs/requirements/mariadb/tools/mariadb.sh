7d0c4158055dc75f3d70dd046780f062428705a6
Mael PIGNET <you@example.com>
2023-06-20 14:09:06 +0200

    test

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -8,5 +8,5 @@ mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWOR
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# exec mysqld_safe
