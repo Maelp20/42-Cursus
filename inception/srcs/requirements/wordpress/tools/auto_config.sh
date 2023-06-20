7e0f0239b29472dad139a893dc27fe0636afdc5c
Mael PIGNET <you@example.com>
2023-06-20 16:27:18 +0200

    wda

--- inception/srcs/requirements/wordpress/tools/auto_config.sh
+++ inception/srcs/requirements/wordpress/tools/auto_config.sh
@@ -7,13 +7,14 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 	echo "Configuring wordpress..."
 	echo "${SQL_DATABASE}<->${SQL_USER}<->${SQL_PASSWORD}"
 	wp config create	--allow-root \
+						--url=mpignet.42.fr \
 						--dbname=$SQL_DATABASE \
 						--dbuser=$SQL_USER \
 						--dbpass=$SQL_PASSWORD \
-						--dbhost='mariadb:3306' \
+						--dbhost=mariadb \
 						--path='/var/www/wordpress'
 sleep 4
-wp core install     --url='mpignet.42.fr' --title='inception' --admin_user=$ADM_U --admin_password=$ADM_P --admin_email='mpignet' --allow-root --path='/var/www/wordpress'
+wp core install     --allow-root --url='mpignet.42.fr' --title='inception' --admin_user=$ADM_U --admin_password=$ADM_P --admin_email='mpignet' --path='/var/www/wordpress'
 wp user create      --allow-root --role=author mpignet mpignet20@gmail.com --user_pass='1234' --path='/var/www/wordpress' >> /log.txt
 fi
 
