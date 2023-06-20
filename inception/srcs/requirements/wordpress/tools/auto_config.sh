b261d5cbb6ea4e4883445670bf70a75937318b73
Mael PIGNET <you@example.com>
2023-06-20 14:57:45 +0200

    adwa

--- inception/srcs/requirements/wordpress/tools/auto_config.sh
+++ inception/srcs/requirements/wordpress/tools/auto_config.sh
@@ -12,7 +12,6 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 						--dbpass=$SQL_PASSWORD \
 						--dbhost=mariadb:3306 --path='/var/www/wordpress'
 sleep 4
-echo "4"
 wp core install     --url=$WEBSITE_N --title=$SITE_T --admin_user=$ADM_U --admin_password=$ADM_P --admin_email=$ADM_E --allow-root --path='/var/www/wordpress'
 wp user create      --allow-root --role=author $FIRST_L $FIRST_M --user_pass=$FIRST_P --path='/var/www/wordpress' >> /log.txt
 fi
