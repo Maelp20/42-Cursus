6f34a933896622d1955a38e5ed4a225fe2f8fadd
Mael PIGNET <you@example.com>
2023-06-20 14:22:24 +0200

    awd

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -11,4 +11,4 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 echo $SQL_ROOT_PASSWORD
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
-echo $SQL_ROOT_PASSWORD
+echo "Passwd is: $SQL_ROOT_PASSWORD"
