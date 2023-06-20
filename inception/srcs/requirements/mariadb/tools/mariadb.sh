61410c0df78b2cfb118db0e243e072626732a9d8
Mael PIGNET <you@example.com>
2023-06-20 14:49:25 +0200

    mariadb ok, debugging wordpress

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -8,7 +8,5 @@ mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWOR
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-echo $SQL_ROOT_PASSWORD
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
-echo "Passwd is: $SQL_ROOT_PASSWORD"
